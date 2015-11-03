b7835a02e4f45a7a48924c80c9c671ab7479551f
drbobdugan <bdugan@stonehill.edu>
2015-11-03 13:02:15 -0500

    Initial Commit

---
+++ UIApplicationBackgroundTaskExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIApplicationBackgroundTaskExample
+//
+//  Created by Bob Dugan on 11/3/15.
+//  Copyright © 2015 Bob Dugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIApplicationBackgroundTaskExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIApplicationBackgroundTaskExample
+//
+//  Created by Bob Dugan on 11/3/15.
+//  Copyright © 2015 Bob Dugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
